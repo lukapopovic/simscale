96ffc04e8a78219403615046f531100d4b850803
Luka Popovic <luka.popovic@teradata.com>
2016-10-19 16:54:38 +0200

    Continuing the work on the case

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -12,6 +12,7 @@ export class ItemComponent implements OnInit {
   FALLBACK_PICTURE: string = '../../assets/fallbackPic.jpg'
   resultThumbnail: string;
   resultId: string;
+  resultDate: string
 
   constructor() { }
 
@@ -19,6 +20,25 @@ export class ItemComponent implements OnInit {
       this.resultId = this.project.resultIds[0];
       this.resultId ? this.resultThumbnail = this.API_URL + this.project.resultThumbnailUrls[this.resultId] 
                       : this.resultThumbnail = this.FALLBACK_PICTURE;
+      this.resultDate = this.parseDate(this.project.lastModificationDate);
   }
 
+  parseDate(date: string): string {
+      var currentDate: any = new Date();
+      var changeDate: any = new Date(date);
+      var yearDifference: number = currentDate.getYear() - changeDate.getYear();
+      var monthDifference: number = currentDate.getMonth() - changeDate.getMonth();
+      var dayDifference: number = currentDate.getDay() - changeDate.getDay();
+      var returnString: string = 'Changed ';
+
+      if(yearDifference > 0) {
+        returnString += yearDifference > 1 ? yearDifference + ' years ago' : yearDifference + ' year ago';
+      } else if (monthDifference > 0) {
+        returnString += monthDifference >= 1 ? monthDifference + ' months ago' : monthDifference + ' month ago';
+      } else {
+        returnString += dayDifference >= 1 ? dayDifference + ' days ago' : dayDifference + ' day ago';
+      }
+
+      return returnString;
+  }
 }
